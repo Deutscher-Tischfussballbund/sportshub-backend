884ba01934d4c0f6d6d5de4b8fe1fb344d108608
Marvin Flock <mflock@flock-it.de>
2025-10-27 23:50:48 +0100

    fix: make runner public for execution from console

--- src/main/java/de/dtfb/sportshub/backend/SportshubBackendApplication.java
+++ src/main/java/de/dtfb/sportshub/backend/SportshubBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SportshubBackendApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(SportshubBackendApplication.class, args);
     }
 
