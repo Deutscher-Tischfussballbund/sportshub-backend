5f9dd2a339d46b38b3a7f4590e0891ace33da887
Marvin Flock <mflock@flock-it.de>
2025-12-26 00:10:32 +0100

    chore: add sql resolve suppression for table not found warning in sql file (table will be created by hibernate)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
+-- noinspection SqlResolveForFile
+
 INSERT INTO season (uuid, name)
 VALUES ('11111111-1111-1111-1111-111111111111', '2026'),
        ('22222222-2222-2222-2222-222222222222', '2025');
