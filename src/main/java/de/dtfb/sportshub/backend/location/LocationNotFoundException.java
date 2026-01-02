16e45fef1c7857b7cffc05a5816c2925708dc34e
Marvin Flock <mflock@flock-it.de>
2026-01-02 20:25:39 +0100

    fix: update location exception message

--- src/main/java/de/dtfb/sportshub/backend/location/LocationNotFoundException.java
+++ src/main/java/de/dtfb/sportshub/backend/location/LocationNotFoundException.java
@@ -3,10 +3,10 @@ package de.dtfb.sportshub.backend.location;
 import de.dtfb.sportshub.backend.exception.NotFoundExceptionMarker;
 
 public class LocationNotFoundException extends NotFoundExceptionMarker {
-    public final String errorCode = "SEASON_NOT_FOUND";
+    public final String errorCode = "LOCATION_NOT_FOUND";
 
     public LocationNotFoundException(String uuid) {
-        super("Could not find season with uuid " + uuid);
+        super("Could not find location with uuid " + uuid);
     }
 
     @Override
