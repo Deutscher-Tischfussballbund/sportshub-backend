4b0969ce6b5248262837c45c0850879fc59310ce
Marvin Flock <mflock@flock-it.de>
2026-06-21 17:14:17 +0200

    chore: remove unused files

--- src/main/java/de/dtfb/sportshub/backend/enums/Side.java
+++
@@ -1,6 +0,0 @@
-package de.dtfb.sportshub.backend.enums;
-
-public enum Side {
-    HOME,
-    AWAY
-}
--- src/main/java/de/dtfb/sportshub/backend/user/AppRole.java
+++
@@ -1,6 +0,0 @@
-package de.dtfb.sportshub.backend.user;
-
-public enum AppRole {
-    ADMIN,
-    CAPTAIN
-}
